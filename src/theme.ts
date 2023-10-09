3f52161ee3a4b5b835ec2922ffed443f26c6091c
Tooni <nsayers@coveo.com>
2023-10-09 10:04:17 +0100

    palatino

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mantine/core";
 export const theme = createTheme({
   primaryColor: "red",
   fontFamily:
-    "Georgia,Garamond,Palatino,Times,Times New Roman,Arial,Helvetica,sans-serif",
+    "Palatino,Georgia,Garamond,Times,Times New Roman,Arial,Helvetica,sans-serif",
   fontSizes: {
     xs: '14px',
     sm: '16px',
