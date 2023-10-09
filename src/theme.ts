95f6e5deb746b205dd96c95df3f067afa7067382
Tooni <nsayers@coveo.com>
2023-10-09 09:51:36 +0100

    change font

--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,13 @@ import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
   primaryColor: "red",
+  fontFamily:
+    "Georgia,Garamond,Palatino,Times,Times New Roman,Arial,Helvetica,sans-serif",
+  fontSizes: {
+    xs: '14px',
+    sm: '16px',
+    md: '18px',
+    lg: '20px',
+    xl: '24px',
+  },
 });
