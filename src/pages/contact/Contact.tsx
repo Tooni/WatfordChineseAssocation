5d89915ed66746baf4661a6c73717848229e0c1e
Neil Sayers <nsayers@coveo.com>
2024-02-17 11:35:55 +0000

    Update Contact.tsx

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -9,10 +9,12 @@ export const Contact: FunctionComponent = () => {
       <Stack>
         <Title order={2}>How to contact us?</Title>
         <Box>
-          <Text>Contact Anita Chan at</Text>
-          <Text>Phone: 07757881088</Text>
-          <Text>Email: <a href = "mailto: anitachanyl@yahoo.co.uk">anitachanyl@yahoo.co.uk</a></Text>
-          <Text>WeChat: Anitachan88</Text>
+          <Text>Contact our Chair, Mrs Anita Chan, at</Text>
+          <List>
+            <List.Item>Phone: 07757881088</List.Item>
+            <List.Item>Email: <a href = "mailto: anitachanyl@yahoo.co.uk">anitachanyl@yahoo.co.uk</a></List.Item>
+            <List.Item>WeChat: Anitachan88</List.Item>
+          </List>
         </Box>
       </Stack>
     </>
