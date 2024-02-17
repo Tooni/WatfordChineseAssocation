057b7b5a3e9fc443c77f345977cf6dcc95173c88
Neil Sayers <nsayers@coveo.com>
2024-02-17 11:41:19 +0000

    Update Gallery.tsx

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -1,4 +1,3 @@
-import { Text } from "@mantine/core";
 import { FunctionComponent } from "react";
 import { SetDocumentTitle } from "../../components/set-document-title/SetDocumentTitle";
 import { ImageGallery } from "../../components/image-gallery/ImageGallery";
