df4c64b487c1d2c3ac159e709083f3bb17941a5a
Tooni <nsayers@coveo.com>
2025-01-18 18:28:59 +0000

    link logo to homepage

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,13 +1,15 @@
-import { Title } from "@mantine/core";
+import { Anchor, Title } from "@mantine/core";
 import { FunctionComponent } from "react";
 
 export const Logo: FunctionComponent = () => {
   return (
-    <Title
-      ff="Georgia,Garamond,Palatino,Times,Times New Roman,Arial"
-      c="gray.2"
-    >
-      Watford Chinese Association
-    </Title>
+    <Anchor href="/" title="Watford Chinese Association">
+      <Title
+        ff="Georgia,Garamond,Palatino,Times,Times New Roman,Arial"
+        c="gray.2"
+      >
+        Watford Chinese Association
+      </Title>
+    </Anchor>
   );
 };
