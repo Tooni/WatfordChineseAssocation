f2a8998c478dc5638e5342cc122e79ee7d9f0daa
Tooni <nsayers@coveo.com>
2025-01-18 18:08:27 +0000

    add news about CNY culture show 2025

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,6 +1,7 @@
-import { Group, List, Stack, Image, Title } from "@mantine/core";
+import { Group, List, Stack, Image, Anchor, Title } from "@mantine/core";
 import { FunctionComponent } from "react";
 import show1BigDance from "../../assets/show_1_big_dance.jpeg";
+import chineseNewYearCultureShow2025 from "../../assets/chinese_new_year_culture_show_2025.jpeg";
 import { SetDocumentTitle } from "../../components/set-document-title/SetDocumentTitle";
 
 export const News: FunctionComponent = () => {
@@ -11,11 +12,16 @@ export const News: FunctionComponent = () => {
         <Stack gap="md">
           <Title order={1}>News</Title>
           <Title order={3}>Events</Title>
+          <Anchor
+            href={chineseNewYearCultureShow2025}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image h={700} src={chineseNewYearCultureShow2025} fit="contain" />
+          </Anchor>
           <Title order={5}>Planned</Title>
           <List listStyleType="lower-alpha">
-            <List.Item>Christmas lunch</List.Item>
-            <List.Item>Chinese New Year Culture Show 2024</List.Item>
-            <List.Item>Chinese New Year Reunion Dinner</List.Item>
+            <List.Item>Chinese New Year Culture Show 2025</List.Item>
           </List>
           <Title order={5}>Past</Title>
           <List listStyleType="lower-alpha">
@@ -24,6 +30,8 @@ export const News: FunctionComponent = () => {
             <List.Item>Logo Competition</List.Item>
             <List.Item>Mid-Autumn Festival Lunch</List.Item>
             <List.Item>Watford Football Stadium Tour</List.Item>
+            <List.Item>Christmas lunch</List.Item>
+            <List.Item>Chinese New Year Reunion Dinner</List.Item>
           </List>
         </Stack>
         <Image h={400} src={show1BigDance} fit="contain" />
