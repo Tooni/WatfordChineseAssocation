e16f117f84f8233443ee48dda8227601bf7d4bcc
Tooni <nsayers@coveo.com>
2025-01-18 20:19:53 +0000

    programmatically get current year in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,10 +2,11 @@ import { Group, Text } from "@mantine/core";
 import { FunctionComponent } from "react";
 
 export const Footer: FunctionComponent = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <Group justify="flex-end" mt="xl">
       <Text size="xs">
-        Copyright © 2025 Watford Chinese Association. All rights reserved.
+        Copyright © {currentYear} Watford Chinese Association. All rights reserved.
       </Text>
     </Group>
   );
