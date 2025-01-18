47ccb70b11aaf2bf0dd62a1aa013b271be160fa8
Tooni <nsayers@coveo.com>
2025-01-18 20:49:21 +0000

    add logo and update favicon

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,16 +1,20 @@
-import { Title, Anchor } from "@mantine/core";
+import { Image, Group, Title, Anchor } from "@mantine/core";
+import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 import { FunctionComponent } from "react";
 
 export const Logo: FunctionComponent = () => {
   return (
     <Anchor component={Link} to="/">
-      <Title
-        ff="Georgia,Garamond,Palatino,Times,Times New Roman,Arial"
-        c="gray.2"
-      >
-        Watford Chinese Association
-      </Title>
+      <Group>
+        <Image src={logo} alt="Watford Chinese Association" h={90} />
+        <Title
+          ff="Georgia,Garamond,Palatino,Times,Times New Roman,Arial"
+          c="gray.2"
+        >
+          Watford Chinese Association
+        </Title>
+      </Group>
     </Anchor>
   );
 };
