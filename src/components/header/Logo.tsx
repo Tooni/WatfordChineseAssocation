9152241cc782a7101024f927020a0dfdf651756f
Neil Sayers <sayers.neil@gmail.com>
2025-01-20 17:08:09 +0000

    fix logo appearance in firefox

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -7,7 +7,7 @@ export const Logo: FunctionComponent = () => {
   return (
     <Anchor component={Link} to="/">
       <Group>
-        <Image src={logo} alt="Watford Chinese Association" h={100} />
+        <Image src={logo} alt="Watford Chinese Association" h={100} w={100} />
         <Title
           ff="Georgia,Garamond,Palatino,Times,Times New Roman,Arial"
           c="gray.2"
