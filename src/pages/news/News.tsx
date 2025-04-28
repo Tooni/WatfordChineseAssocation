596d1cc3322e86ae5d62a49cdf064ee9fddfe087
Tooni <nsayers@coveo.com>
2025-04-28 21:58:39 +0100

    add more news

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -3,6 +3,7 @@ import { FunctionComponent } from "react";
 import show1BigDance from "../../assets/show_1_big_dance.jpeg";
 import cny2025Second from "../../assets/cny_2025_2.jpeg";
 import cny2025Third from "../../assets/cny_2025_3.jpeg";
+import chineseCultureShow from "../../assets/chinese_new_year_culture_show_2025_2.jpeg"
 import { SetDocumentTitle, Article } from "../../components";
 
 export const News: FunctionComponent = () => {
@@ -12,6 +13,51 @@ export const News: FunctionComponent = () => {
       <Group p={0} justify="space-between">
         <Stack gap="md">
           <Title order={1}>News</Title>
+          <Title order={4}>28/04/2025 – Sold-out Chinese New Year Culture event a resounding success at
+          Watford Palace Theatre.</Title>
+          <Article>
+          <Article.Image src={chineseCultureShow} fit="contain" />
+            <Article.Text>
+              Anita Chan, chair of the Watford Chinese Association said: "This
+              Snake year's Chinese Culture Show was a success. The energy in the
+              auditorium was electric, and it is clear that an event like this
+              brought our community closer together. The night party showcased a
+              variety of traditional art which had touched innumerable people
+              with its profound cultural heritage performances. It is wonderful
+              that the Spring Festival brought people together to appreciate
+              Chinese cultural traditions, regardless of their backgrounds."
+            </Article.Text>
+            <Article.Text>
+              The show included fantastic performances by Lijun Bi and other
+              performers to celebrate the start of the Year of the Snake. This
+              show was curated by Lijun Bi.
+            </Article.Text>
+            <Article.Text>
+              The association has organised the Chinese New Year Culture Show at
+              the Watford Palace Theatre since 2015. The programme includes
+              Chinese painting demonstrations, tea ceremonies, lion dances,
+              Chinese dances, songs, music, magic, acrobats, marshal arts and
+              Bian Lian ("Face-Changing"). Performers include professionals and
+              local talents. The Theatre has 500 seats and tickets have sold out
+              every year. The Association enjoys strong support from our members
+              and the community.
+            </Article.Text>
+            <Article.Text>
+              The association also organises other activities in town, these
+              include: Putonghua, calligraphy, dancing, singing, chi gong,
+              health talks and exercises.
+            </Article.Text>
+            <Article.Text>
+              Elected Mayor of Watford, Peter Taylor, said: "It was great
+              attending the Chinese New Year cultural show recently — a vibrant
+              celebration of culture and tradition. A special thank you to the
+              Watford Chinese Association for organising such a fantastic event.
+              Our town is so privileged to have so many different communities
+              living here in Watford, and this rich diversity is what makes our
+              town so unique."
+            </Article.Text>
+          </Article>
+          <Title order={4}>20/04/2025 – Highstreet lighting ceremonies for Chinese New Year</Title>
           <Article>
             <Article.Text>
               To celebrate the Chinese New Year 2025 and the Watford Chinese
@@ -36,7 +82,7 @@ export const News: FunctionComponent = () => {
             <Image maw={450} src={cny2025Second} />
             <Image maw={450} src={cny2025Third} />
           </Group>
-          <Title order={1}>Past Events</Title>
+          <Title order={3}>Past Events</Title>
           <List listStyleType="lower-alpha">
             <List.Item>Chinese New Year Culture Show 2025</List.Item>
             <List.Item>Dragon Boat Festival Lunch</List.Item>
