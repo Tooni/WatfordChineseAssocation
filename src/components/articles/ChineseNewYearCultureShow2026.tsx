bbbf04764b055f1e09aead6365b302bde264c2a5
Tooni <nsayers@coveo.com>
2026-01-09 15:20:28 +0000

    chore: more consistent file names

--- src/components/articles/ChineseNewYearCultureShow2026.tsx
+++ src/components/articles/ChineseNewYearCultureShow2026.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import { Image, Group } from "@mantine/core";
-import cnyCultureShowLeaflet1 from "../../assets/chinese_new_year_culture_show_2026.jpeg";
+import cnyCultureShowLeaflet1 from "../../assets/chinese_new_year_culture_show_2026_1.jpeg";
 import cnyCultureShowLeaflet2 from "../../assets/chinese_new_year_culture_show_2026_2.jpeg";
 
 export const ChineseNewYearCultureShow2026: FunctionComponent = () => (
