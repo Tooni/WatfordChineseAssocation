7e19935cdaf752d9e8a549b45b9d5de4242c56b9
Tooni <nsayers@coveo.com>
2026-01-20 15:53:23 +0000

    fix iframe

--- src/pages/location/Location.tsx
+++ src/pages/location/Location.tsx
@@ -17,7 +17,7 @@ export const Location: FunctionComponent = () => {
           <Text>WD17 1BJ</Text>
         </Box>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1237.5740057443809!2d-0.3984285608359023!3d51.65712639296199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48766ac151c1488f%3A0x5879b314c72cb7fa!2sWatford%20Palace%20Theatre!5e0!3m2!1sen!2suk!4v1696839604247!5m2!1sen!2suk"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2475.1373352077!2d-0.4006096233709328!3d51.6573218718478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48766ac169f1ce7d%3A0x25f62b53c279e996!2sThe%20Barn!5e0!3m2!1sen!2suk!4v1768924365918!5m2!1sen!2suk"
           width="100%"
           height="450px"
           style={{
